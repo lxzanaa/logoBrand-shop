9906b6ccf6eaf90d438e89739886842969cc175c
lxzanaa <zari.weone2010@gmail.com>
2024-05-21 22:42:20 +0300

    end

--- public/js/paralax.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const parallaxElements = document.querySelectorAll('[data-parallax="scroll"]');
-    parallaxElements.forEach(element => {
-        new Parallax(element);
-    });
-});
